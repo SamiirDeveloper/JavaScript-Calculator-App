fd02259543db84c3adeb63e6b9caae60339221a0
Samir <samirstown@hotmail.com>
2025-07-21 21:42:06 -0700

    Made entering a new number after operation clear the screen

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-//  calculator program to perform basic arithmetic operations
-
 const display = document.getElementById('display');
+let shouldResetDisplay = false;
 
-function appendToDisplay(input) {   
+function appendToDisplay(input) {
+    if (shouldResetDisplay) {
+        display.value = '';
+        shouldResetDisplay = false;
+    }
     display.value += input;
 }
 
 function clearDisplay() {
     display.value = '';
+    shouldResetDisplay = false;
 }
 
 function calculate() {
     try {
         display.value = eval(display.value);
-    } catch (error) {   
+        shouldResetDisplay = true;
+    } catch (error) {
         display.value = 'Error';
+        shouldResetDisplay = true;
     }
-}   
+}
